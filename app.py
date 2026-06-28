0a848bfb8c93a08c4c3e72fdfbad869f19f2991a
malviyalata <87931238+malviyalata@users.noreply.github.com>
2026-06-28 22:56:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    return None
-
+   
 @app.route('/health')
 def health():
     return 'Server is up and running'
